19b22416f583f746402eaf2bf83d4a8c32893bfc
sf314 <sf314@icloud.com>
2016-11-17 19:50:40 -0700

    Does a thing

--- redo/redo.ino
+++ redo/redo.ino
@@ -5,11 +5,15 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <IntersemaBaro.h>
 
 File dataFile;
-int sdPin = 10;
-
+int sdPin = 10; // 10 on Uno
+int heatPin = 3;
 int logNumber = 1;
+
+Intersema::BaroPressure_MS5607B baro(true);
+
 
 // ***** Keep track of time!
 unsigned long previousTime = 0;
@@ -19,8 +23,10 @@ void setup() {
     Serial.begin(9600);
     Wire.begin();
     SPI.begin();
+    baro.init();
 
     pinMode(sdPin, OUTPUT);
+    pinMode(heatPin, OUTPUT);
 
     int tries = 5;
     bool usingSD = false;
@@ -33,14 +39,20 @@ void setup() {
         tries--;
         delay(1000);
     } // Should be set up now... if not, then problems.
-    if (usingSD) {Serial.println("Using SD!!!!!");}
+    if (usingSD) {
+        Serial.println("Using SD");
+    } else {
+        Serial.println("Not using SD");
+    }
 
     // **** Create file headers
-    dataFile = SD.open("sdt.csv", FILE_WRITE); // later, just call with filename
+    dataFile = SD.open("sdt4.csv", FILE_WRITE); // later, just call with filename
     if (dataFile) {
         Serial.println("File opened successfully");
         dataFile.println("Col1, Col2, Col3, Col4, Col5");
         dataFile.close();
+    } else {
+        Serial.println("setup(): dataFile is false");
     }
 }
 
@@ -56,16 +68,24 @@ void loop() {
 
 
 
-        dataFile = SD.open("sdt.csv", FILE_WRITE); // ***** Open file, with options
-        dataFile.println(logNumber);
+        dataFile = SD.open("sdt4.csv", FILE_WRITE); // ***** Open file, with options
+        dataFile.print(logNumber);
         dataFile.print(",");
 
         dataFile.print(readTemp(0));
         dataFile.print(",");
 
         dataFile.print(readTemp(1));
+        dataFile.print(",");
+
+        dataFile.print(readTemp(2));
+        dataFile.print(",");
+
+        dataFile.println(baro.getHeightCentiMeters());
+
         dataFile.close();
         logNumber++;
+        analogWrite(heatPin, 255);
     }
 
 }
@@ -75,8 +95,7 @@ void loop() {
 
 // Read temperature
 double readTemp(int tempPin) {
-    double voltage = 5000
-    double pinMillivolts = analogRead(tempPin) * ((double)voltage / 1024);
+    double pinMillivolts = analogRead(tempPin) * (5000.0 / 1024.0);
     double celsius = (pinMillivolts - 500) / 10; // in celsius
 
     // perform other calculations, or switch on a thing to use Fahrenheit
